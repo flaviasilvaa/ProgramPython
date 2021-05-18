95568f05dd6ec19575b9a4eaf90a0f9af58c9ce1
flaviasilvaa <flaviasilvaa@gmail.com>
2021-05-18 17:05:59 +0100

    Update 65.BigSmallNumbers.py

--- 65.BigSmallNumbers.py
+++ 65.BigSmallNumbers.py
@@ -4,7 +4,15 @@ total = add = average = 0
 while answer in 'Y/y':
     number = int(input('Type a number?\n'))
     add += number
-    total += 1
+    quantity += 1
+    if quantity == 1:
+        biggest = smallest = number
+    else:
+        if number > biggest:
+            biggest = number
+        if number < smallest:
+            smallest = number
     answer = str(input('Do you want to continue?[Y/N]\n')).strip().upper()[0]
-average = add / total
-print(f'You inserted  {total} numbers and the average between them is {average}')
+average = add/ quantity
+print(f'You inserted  {quantity} numbers and the average between them is {average}')
+print(f'The smallest number is {smallest} and the biggest number is {biggest}')
