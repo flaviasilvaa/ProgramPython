aea911fc35ed76b9a2cd330c10e5e80e5fdc57bc
flaviasilvaa <flaviasilvaa@gmail.com>
2021-10-21 21:42:24 +0100

    basic function

---
+++ 85.FunctionGlobalVariable.py
@@ -0,0 +1,15 @@
+##this program is going to create an introduction of someone but with a global variable
+
+name = "Ben"
+city = "New York"
+
+def introduction():
+    print("my name is: ", name)
+    print("I live in ", city)
+
+introduction()
+
+name = "Maria"
+city = "Dublin"
+
+introduction()
